697c41b8a94abe8375e831901a3e74eb43691515
Sven Kirmess <sven.kirmess@kzone.ch>
2020-08-23 02:09:58 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+GitHub action to install cpanm and local::lib and configure local::lib for
+the current workflow.
---
+++ bin/configure_l�ocal_lib.pl
@@ -0,0 +1,26 @@
+#!perl
+
+use 5.006;
+use strict;
+use warnings;
+
+use File::Spec;
+
+use lib File::Spec->catdir( $ENV{HOME}, 'perl5/lib/perl5/' );
+
+my @vars = qw(PERL_MB_OPT PERL_MM_OPT PERL5LIB PATH PERL_LOCAL_LIB_ROOT);
+
+my %old_ENV;
+@old_ENV{@vars} = @ENV{@vars};
+
+require local::lib;
+local::lib->import;
+
+for my $var (@vars) {
+    next if !exists $ENV{$var};
+    next if exists $old_ENV{$var} && $old_ENV{$var} eq $ENV{$var};
+
+    print "::set-env name=${var}::$ENV{$var}\n";
+}
+
+# vim: ts=4 sts=4 sw=4 et: syntax=perl
